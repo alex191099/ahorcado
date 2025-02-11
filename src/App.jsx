9febbfabab1a4d62b16cfa99a16b24745f2245db
AlejandroProfesor <alejandro.gmartin@cesjuanpablosegundo.es>
2025-02-11 10:10:18 +0100

    empezamos conlos dibujos y a plantear el problema lógico de este juego y demás...

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import './App.css'
-
+import { Dibujoscolgao } from './dibujos/dibujoscolgao'
+import { palabras } from './palabras/palabras'
 function App() {
 
+  /* Errores */
+  
+  /* Palabras */
+  const palabrasecreta = palabras[Math.floor(Math.random() * palabras.length)]
   return (
     <>
-
-          +---+
-           |   |
-           O   |
-          /|\  |
-          / \  |
-               |
-          =========
+        <pre>
+          <Dibujoscolgao errores={"6"}></Dibujoscolgao>
+          <p className='dibujoscolgao'>{palabrasecreta}</p>
+        </pre>
     </>
   )
 }
